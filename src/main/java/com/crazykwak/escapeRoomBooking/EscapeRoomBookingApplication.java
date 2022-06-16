91ed646ee07caca4233ce8484679674e611e727a
Crazykwak <kwj1830@naver.com>
2022-06-16 20:38:26 +0900

    first commit

---
+++ src/main/java/com/crazykwak/escapeRoomBooking/EscapeRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package com.crazykwak.escapeRoomBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeRoomBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crazykwak/escapeRoomBooking/EscapeRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazykwak.escapeRoomBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
